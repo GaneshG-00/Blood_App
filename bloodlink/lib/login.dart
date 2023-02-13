29b790cb9b8290da7cae9890ac53a1cf8053ccf2
GaneshG-00 <ganesh.g30.3.2003@gmail.com>
2023-02-13 20:59:22 +0530

    second commit

--- bloodlink/lib/login.dart
+++ bloodlink/lib/login.dart
@@ -1,45 +1,70 @@
 import 'package:bloodlink/register.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 80,),
-            Image.asset('assets/images/logo1.jpg',height: 200,width: 200,),
-            SizedBox(height: 20,),
-            Text('SIGN IN',style: TextStyle(color: Color.fromARGB(255, 162, 28, 19),fontSize:30,fontWeight: FontWeight.w900),),
-            SizedBox(height: 80),
-            Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                          hintText: 'Username'))),
-                           SizedBox(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 116, 17, 10),
+        body: Padding(
+          padding:
+              const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 50),
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.white,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo1.jpg',
+                    height: 150,
+                    width: 150,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 120, 118, 118),
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 60),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Username'),
+                        style: TextStyle(height: 1),
+                      )),
+                  const SizedBox(
                     height: 20,
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 20, right: 20),
                     child: TextField(
                       decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                          hintText: 'Password'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        hintText: 'Password',
+                      ),
+                      style: TextStyle(height: 1),
                     ),
                   ),
                   const SizedBox(
-                    height: 60,
+                    height: 30,
                   ),
                   ElevatedButton(
                       onPressed: () {},
@@ -48,29 +73,29 @@ class Login extends StatelessWidget {
                               RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(60)))),
                       child: const Text('  Login  ')),
-                      const SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("doesn't have an account"),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) => const Register())));
-                          },
-                          child: const Text(
-                            'Register Now',
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 103, 175, 234)),
-                          )
-                          ),
-                          ]
-                          )
-          ],
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    const Text("doesn't have an account"),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) =>
+                                      const RegisterFirst())));
+                        },
+                        child: const Text(
+                          'Register Now',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 103, 175, 234)),
+                        )),
+                  ])
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
--- bloodlink/lib/register.dart
+++ bloodlink/lib/register.dart
@@ -1,14 +1,179 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class Register extends StatelessWidget {
-  const Register({super.key});
+class RegisterFirst extends StatelessWidget {
+  const RegisterFirst({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Register here',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 120, 118, 118),
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Username'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Create password'))),
+                SizedBox(height: 15),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Confirm password'))),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(60)))),
+                        child: const Text('     Back     ')),
+                    SizedBox(width: 60),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) =>
+                                      const RegisterSecond())));
+                        },
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(60)))),
+                        child: const Text('     Next     ')),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterSecond extends StatelessWidget {
+  const RegisterSecond({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Enter Your Details',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 120, 118, 118),
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Full name'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'E-mail Id'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Mobile Number'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'State'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'District'))),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            hintText: 'Pincode'))),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

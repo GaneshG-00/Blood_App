ad24879847fbb90abbd9e59aa298288b9db15b6f
GaneshG-00 <ganesh.g30.3.2003@gmail.com>
2023-03-02 18:35:31 +0530

    post firestore push

--- bloodlink/lib/main.dart
+++ bloodlink/lib/main.dart
@@ -7,7 +7,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import 'alfi.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
